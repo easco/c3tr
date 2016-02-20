37e7cb3afaa63d24e9141b7fc6ab44f5829a3e79
Josh Forisha <josh@forisha.com>
2016-02-19 20:17:01 -0600

    Add Random.from()

--- app/Random.js
+++ app/Random.js
@@ -1,15 +1,24 @@
 
 // Data ------------------------------------------------------------------------
 
-const almostOne = 1 - 1e-16;
+const almostOne = 1 - 1e-16; // The highest float less than 1
 
 // Exports ---------------------------------------------------------------------
 
 module.exports = {
+  from,
   integer
 };
 
 // Functions -------------------------------------------------------------------
+
+function from(enumerable) {
+  if (Array.isArray(enumerable)) {
+    return enumerable[integer(0, enumerable.length - 1)];
+  }
+
+  return enumerable[from(Object.keys(enumerable)];
+}
 
 function integer(min = 0, max = Number.MAX_VALUE) {
   return Math.floor(Math.random() * (max - min + almostOne) + min);
