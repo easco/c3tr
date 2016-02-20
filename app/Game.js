0dc7b55d01dc3be2e42febc50fdabf2957cee345
Josh Forisha <josh@forisha.com>
2016-02-19 20:04:49 -0600

    Add stylesheet, Game module

---
+++ app/Game.js
@@ -0,0 +1,19 @@
+
+// Data ------------------------------------------------------------------------
+
+// Exports ---------------------------------------------------------------------
+
+module.exports = {
+  init
+};
+
+// Functions -------------------------------------------------------------------
+
+function init() {
+  // TODO: Begin main game loop
+
+  window.addEventListener('keydown', event => {
+    // TODO: Handle keydown event
+    console.log('Keydown:', event);
+  });
+}
--- app/index.js
+++ app/index.js
@@ -1,3 +1,3 @@
-window.addEventListener('load', () => {
-  console.log('Loaded!');
-});
+import { init } from 'Game';
+
+window.addEventListener('load', init);
