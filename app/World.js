b69de48abc8013970b0b2008d70aac4344b42f82
Josh Forisha <josh@forisha.com>
2016-02-19 22:38:09 -0600

    Add World module

---
+++ app/World.js
@@ -0,0 +1,24 @@
+import FastSimplexNoise from 'fast-simplex-noise';
+
+// Data ------------------------------------------------------------------------
+
+// Exports ---------------------------------------------------------------------
+
+module.exports = {
+};
+
+// Functions -------------------------------------------------------------------
+
+function generate(width, height) {
+  const wallNoise = new FastSimplexNoise();
+  const grid = new Array(width);
+
+  for (let x = 0; x < width; x++) {
+    grid[x] = new Array(height);
+    for (let y = 0; y < height; y++) {
+      grid[x][y] = wallNoise.in2D(x, y);
+    }
+  }
+
+  return grid;
+}
