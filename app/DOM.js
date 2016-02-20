5bddd88886771b1c4b026e1996cc38452bb04809
Josh Forisha <josh@forisha.com>
2016-02-19 20:26:18 -0600

    Add DOM module

---
+++ app/DOM.js
@@ -0,0 +1,38 @@
+
+// Data ------------------------------------------------------------------------
+
+// Exports ---------------------------------------------------------------------
+
+export default {
+  find,
+  html,
+  text
+};
+
+// Functions -------------------------------------------------------------------
+
+function find(selector, context = document) {
+  return context.querySelector(selector);
+}
+
+function html(type, attributes, children) {
+  const element = document.createElement(type);
+
+  Object.keys(attributes).forEach(name => {
+    element.setAttribute(name, attribute[name]);
+  });
+
+  children.map(
+    child =>
+      typeof child === 'number' || typeof child === 'string'
+      ? text(child)
+      : child
+    )
+    .forEach(child => element.appendChild(child));
+
+  return element;
+}
+
+function text(content) {
+  return document.createTextNode(content);
+}
