0e8fff166443bd59bc47b89f5cb6ac48cb892e3b
Josh Forisha <josh@forisha.com>
2016-02-23 15:47:40 -0600

    Improve Status panel information

--- app/Text.js
+++ app/Text.js
@@ -1,14 +1,18 @@
 
-// Data ------------------------------------------------------------------------
-
 // Exports ---------------------------------------------------------------------
 
 export default {
-  number
+  capitalize,
+  formatNumber
 };
 
 // Functions -------------------------------------------------------------------
 
-function number(num) {
+function capitalize(text) {
+  if (!text.hasOwnProperty('length') || text.length < 1) return '';
+  return text.slice(0, 1).toUpperCase() + text.slice(1);
+}
+
+function formatNumber(num) {
   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
--- app/views/status.js
+++ app/views/status.js
@@ -1,7 +1,13 @@
 import Carl from 'entities/Carl';
 import DOM from 'DOM';
+import Text from 'Text';
+import World from 'World';
 
-export default function renderStatus({ state }) {
+const sep = t => DOM.html('span.sep', [DOM.text(t)]);
+const stat = c => DOM.html('span.stat', c);
+const unit = t => DOM.html('span.unit', [DOM.text(t)]);
+
+export default function renderStatus({ state, world }) {
   const carl = Carl.findIn(state.entities);
 
   const energySpan = DOM.html('span.energy');
@@ -11,16 +17,26 @@ export default function renderStatus({ state }) {
   else if (energyLevel < 50) energySpan.classList.add('-low');
 
   const statusEl = DOM.html('div#Status', [
-    DOM.html('span.stats', [
-      `${carl.health.current}/${carl.health.max}HP`,
-      DOM.html('span.stat', [
-        `CPU: ${carl.cpuSlot.cpu.clockSpeed.toFixed(1)}GHz`
+    DOM.html('section.-left', [
+      stat([
+        'HP', sep(':'),
+        carl.health.current, sep('/'), carl.health.max
       ]),
-      DOM.html('span.stat', [
-        `(${carl.position.x + 1}, ${carl.position.y + 1})`
+      stat([
+        'CPU', sep(':'),
+        carl.cpuSlot.cpu.clockSpeed.toFixed(1), unit('GHz')
       ])
     ]),
-    DOM.html('span.energy-bar', [energySpan])
+    DOM.html('section.-center', [
+      `(${carl.position.x + 1}, ${carl.position.y + 1}) `,
+      sep(Text.capitalize(World.tileAt(world, carl.position)))
+    ]),
+    DOM.html('section.-right', [
+      stat([
+        Text.formatNumber(carl.energy.current), unit('J')
+      ]),
+      DOM.html('span.energy-bar', [energySpan])
+    ])
   ]);
   document.body.replaceChild(statusEl, DOM.find('#Status'));
 }
